915172c6dc8a3011826cef2ec37c9ed8b763b345
pallabofficial <officialpallabmondal000@gmail.com>
2024-02-15 19:58:17 +0530

    added theme change mode: added a button which gives option to change modes from light to deep

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+let main = document.querySelector("#main");
+let changeMode = document.querySelector("#change-mode");
+let navItems = document.querySelectorAll(".navitems");
+let navLinks = document.querySelectorAll(".navitems a");
+let navItemAnim = document.querySelector(".navitems::after");
+
+let alternate = "white";
+changeMode.addEventListener("click",()=> {
+    if(alternate==="white"){
+        main.style.background = "linear-gradient(to right, #0B1120, #0B1A2D)";
+        navItems.forEach((navItemNo)=>{
+            navItemNo.style.borderColor = "white";
+        })
+        navLinks.forEach((navItemNo)=>{
+            navItemNo.style.color = "white";
+        })
+        main.style.color = "white";
+        alternate = "black";
+    }
+    else{
+        navItems.forEach((navItemNo)=>{
+            navItemNo.style.borderColor = "rgba(0, 0, 0, 0.153)";
+        })
+        navLinks.forEach((navItemNo)=>{
+            navItemNo.style.color = "black";
+        })
+        main.style.background = "linear-gradient(#efeae3,#efeae3)";
+        main.style.color = "black";
+        alternate = "white";
+    }
+    
+})
